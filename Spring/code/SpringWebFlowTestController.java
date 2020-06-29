2ba6920de53bedf9053a298e3d085a336ce5896c
feihong <huangfeihong-cs@163.com>
2020-06-29 16:29:36 +0800

    为Spring-webflow 回显代码增加注释

--- Spring/code/SpringWebFlowTestController.java
+++ Spring/code/SpringWebFlowTestController.java
@@ -15,6 +15,12 @@ public class SpringWebFlowTestController {
     @GetMapping("/echo")
     public String test() throws IOException {
 
+        //自己搭建的环境测试不成功，ExternalContextHolder.getExternalContext() 返回 null，可能是环境配置的不对
+        //依赖：spring-webflow.jar
+        //参考：
+        //  1. https://www.00theway.org/2020/01/04/apereo-cas-rce/
+        //  2. https://www.programcreek.com/java-api-examples/?class=org.springframework.webflow.context.ExternalContextHolder&method=getExternalContext
+
         org.springframework.webflow.context.servlet.ServletExternalContext servletExternalContext = (org.springframework.webflow.context.servlet.ServletExternalContext) org.springframework.webflow.context.ExternalContextHolder.getExternalContext();
         javax.servlet.http.HttpServletRequest request = (javax.servlet.http.HttpServletRequest) servletExternalContext.getNativeRequest();
         javax.servlet.http.HttpServletResponse response = (javax.servlet.http.HttpServletResponse) servletExternalContext.getNativeResponse();
